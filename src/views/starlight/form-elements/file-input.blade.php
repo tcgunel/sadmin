78d588016e1431bff197f06c21c7d781381dc9a8
Ömer Kamçılı <omerkamcili@gmail.com>
2020-01-02 15:50:07 +0300

    Changes for multiple file input

--- src/views/starlight/form-elements/file-input.blade.php
+++ src/views/starlight/form-elements/file-input.blade.php
@@ -3,7 +3,7 @@
     <div class="col-lg-9 col-md-8 mg-t-10 mg-sm-t-0">
         <input
                 id="{{ $row->id ? $row->id : uniqid() }}"
-                name="{{ $row->name }}"
+                name="{{ $row->name  . ($row->multiple ? '[]' : '') }}"
                 type="file"
                 {{ $row->multiple ? ' multiple' : '' }}
                 class="form-control @if(is_array($row->class)){{ implode(' ',$row->class) }} @elseif(is_string($row->class)) {{ $row->class }} @endif {{ $errors->get($row->name) ? 'is-invalid' : '' }}"
@@ -11,17 +11,38 @@
                 value="{{ $row->value ? $row->value : (old($row->name) ? old($row->name) : '') }}"
                 @if($row->required)required @endif>
 
-        @if(isset($errors) && $errors->get($row->name))
 
-            <small class="form-text text-danger">
+        @if(isset($errors))
 
-                @foreach($errors->get($row->name) as $message)
-                    {{ $message }}
-                @endforeach
+            @if(!$row->multiple && $errors->get($row->name))
 
-            </small>
+                <small class="form-text text-danger">
 
-        @elseif($row->description)
+                    @foreach($errors->get($row->name) as $message)
+                        {{ $message }}
+                    @endforeach
+
+                </small>
+
+            @else
+
+                <small class="form-text text-danger">
+
+                    @foreach($errors->get($row->name . '.*') as $fileErrors)
+
+                        @foreach($fileErrors as $message)
+                            {{ $message }}
+                        @endforeach
+
+                    @endforeach
+
+                </small>
+
+            @endif
+
+        @endif
+
+        @if($row->description)
 
             <small class="form-text text-info">
                 {{  $row->description }}
