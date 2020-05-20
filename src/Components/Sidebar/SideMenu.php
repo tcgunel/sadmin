04ab870d6a55370fd2b85e222c9865636e1f9005
Ömer Kamçılı <omerkamcili@gmail.com>
2020-05-20 10:31:23 +0300

    SideMenu changes

--- src/Components/Sidebar/SideMenu.php
+++ src/Components/Sidebar/SideMenu.php
@@ -4,6 +4,7 @@ namespace OmerKamcili\Sadmin\Components\Sidebar;
 
 use ArrayIterator;
 use IteratorAggregate;
+use OmerKamcili\Sadmin\Components\Interfaces\MenuItemInterface;
 
 /**
  * Class SideMenu
@@ -21,19 +22,11 @@ class SideMenu implements IteratorAggregate
     /**
      * @param MenuItem $item
      */
-    public function add(MenuItem $item)
+    public function add(MenuItemInterface $item)
     {
 
         $this->items[] = $item;
 
-    }
-
-    /**
-     * @param MenuGroup $group
-     */
-    public function addGroup(MenuGroup $group)
-    {
-        $this->items[] = $group;
     }
 
     /**
